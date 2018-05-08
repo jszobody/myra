24bd391af5a6123f9cedcafb6c103dc9c20fc23d
Joseph Szobody <joseph@stechstudio.com>
2018-05-07 20:20:26 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="de">
+<head>
+<title>Myra Winona Szobody</title>
+<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+<link rel="stylesheet" title="Standard" href="screen.css" type="text/css" media="screen" />
+<script language="JavaScript" type="text/javascript" src="imageflow.js"></script>
+</head>
+<body>
+
+
+<div id="imageflow"> 
+	<div id="loading">
+		<b>Loading images</b><br/>
+		<img src="loading.gif" width="208" height="13" alt="loading" />
+	</div>
+	<div id="images">
+		<img src="reflect.php?img=img/1.jpg&bgc=F6F0EE"/>
+		<img src="reflect.php?img=img/2.jpg&bgc=F6F0EE"/>
+		<img src="reflect.php?img=img/3.jpg&bgc=F6F0EE"/>
+		<img src="reflect.php?img=img/4.jpg&bgc=F6F0EE"/>
+		<img src="reflect.php?img=img/5.jpg&bgc=F6F0EE"/>
+		<img src="reflect.php?img=img/6.jpg&bgc=F6F0EE"/>
+		<img src="reflect.php?img=img/7.jpg&bgc=F6F0EE"/>
+		<img src="reflect.php?img=img/15.jpg&bgc=F6F0EE"/>
+		<img src="reflect.php?img=img/8.jpg&bgc=F6F0EE"/>
+		<img src="reflect.php?img=img/9.jpg&bgc=F6F0EE"/>
+		<img src="reflect.php?img=img/10.jpg&bgc=F6F0EE"/>
+		<img src="reflect.php?img=img/11.jpg&bgc=F6F0EE"/>
+		<img src="reflect.php?img=img/12.jpg&bgc=F6F0EE"/>
+		<img src="reflect.php?img=img/13.jpg&bgc=F6F0EE"/>
+		<img src="reflect.php?img=img/14.jpg&bgc=F6F0EE"/>
+	</div>
+	<div id="captions"></div>
+	<div style="background-color: #8B7473;">
+	<div id="scrollbar">
+		<div id="slider"></div>
+	</div>
+</div>
+</div>
+<img src="img/top2.gif" style="position: absolute; margin-top: 15px;"/>
+<img src="img/top.gif" style="position: absolute; margin-top: 5%;"/>
+<div class="clear"></div>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-1691241-8");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
+</body>
+</html>
